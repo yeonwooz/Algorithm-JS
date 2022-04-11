29ed617347e45eccb75529911137c5ea7e8a25d3
suyeon woo <woosean999@gmail.com>
2022-04-11 18:00:35 +0900

    해시맵 - 각 후보가 얻은 표수 - Map 사용

--- src/hash/vote.ts
+++ src/hash/vote.ts
@@ -6,7 +6,7 @@ type Result = {
   [key: string]: number
 }
 
-function solution(papers: string, candidates: string[]) {
+function solution1(papers: string, candidates: string[]) {
   const results: Result = {}
 
   for (let candidate of candidates) {
@@ -17,17 +17,37 @@ function solution(papers: string, candidates: string[]) {
     results[paper]++
   }
 
-  console.log(333333, results)
-
   let elected = 'A'
 
   for (let result in results) {
-      if (results[result] > results[elected]) {
-        elected = result
-      }
+    if (results[result] > results[elected]) {
+      elected = result
+    }
   }
 
   return elected
 }
 
-export { solution as vote }
+function solution2(chosenCandidates: string, candidates: string[]): string {
+  const results = new Map<string, number>()
+
+  for (let candidate of candidates) {
+    results.set(candidate, 0)
+  }
+
+  for (let chosenCandidate of chosenCandidates) {
+    const prevVote = results.get(chosenCandidate)
+    results.set(chosenCandidate, prevVote + 1)
+  }
+
+  let elected = candidates[0]
+  for (let result of results) {
+    if (result[1] > results.get(elected)) {
+      elected = result[0]
+    }
+  }
+
+  return elected
+}
+
+export { solution2 as vote }
