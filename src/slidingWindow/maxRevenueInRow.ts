5ba71f3a39bef65eb49a90c6565023b51450c8d6
suyeon woo <woosean999@gmail.com>
2022-04-12 13:31:28 +0900

    k보다 작을때와 클 때로 구간 분기

--- src/slidingWindow/maxRevenueInRow.ts
+++ src/slidingWindow/maxRevenueInRow.ts
@@ -20,4 +20,25 @@ function solution(
   return maxRevenue
 }
 
-export { solution as getMaxRevenue }
+// 연산 재사용
+function solution2(
+  n: number, // 5 <= n <= 100,000
+  k: number, // 2 <= k <= n
+  revenues: number[] // 0 <= each number <= 500
+): number {
+  let answer,
+    sum = 0 // comma operator
+
+  for (let i = 0; i < k; ++i) {
+    sum += revenues[i]
+  }
+  answer = sum
+
+  for (let i = k; i < n; ++i) {
+    sum += revenues[i] - revenues[i - k]
+    answer = Math.max(answer, sum)
+  }
+  return answer
+}
+
+export { solution2 as getMaxRevenue }
