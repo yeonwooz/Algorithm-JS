1471273365ce2d92ef798679eeede060c11aab1c
suyeon woo <woosean999@gmail.com>
2022-04-13 11:20:06 +0900

    카카오 크레인 - answer 를 2씩 증가

--- src/stack/craneGame.ts
+++ src/stack/craneGame.ts
@@ -4,40 +4,39 @@ https://programmers.co.kr/learn/courses/30/lessons/64061?language=javascript
 */
 
 function solution(board: number[][], moves: number[]): number {
-    let answer = 0
-    // board 이차원배열은 위쪽부터 아래로 가로배열들의 배열
-  
-    const dollsStack = []
-    let topDollInStack = 0
-  
-    let i = 0
-    while (i < moves.length) {
-      const pickedLineIndex = moves[i] - 1
-      for (let j = 0; j < board.length; ++j) {
-        const targetDoll = board[j][pickedLineIndex]
-  
-        if (targetDoll > 0) {
-          if (topDollInStack === targetDoll) {
-            answer++
-            dollsStack.pop()
-            topDollInStack = dollsStack[dollsStack.length - 1]
-          } else {
-            dollsStack.push(targetDoll)
-            topDollInStack = targetDoll
-          }
-  
-          board[j][pickedLineIndex] = 0
-          break
+  let answer = 0
+  // board 이차원배열은 위쪽부터 아래로 가로배열들의 배열
+
+  const dollsStack = []
+  let topDollInStack = 0
+
+  let i = 0
+  while (i < moves.length) {
+    const pickedLineIndex = moves[i] - 1
+    for (let j = 0; j < board.length; ++j) {
+      const targetDoll = board[j][pickedLineIndex]
+
+      if (targetDoll > 0) {
+        if (topDollInStack === targetDoll) {
+          answer += 2
+          dollsStack.pop()
+          topDollInStack = dollsStack[dollsStack.length - 1]
+        } else {
+          dollsStack.push(targetDoll)
+          topDollInStack = targetDoll
         }
+
+        board[j][pickedLineIndex] = 0
+        break
       }
-  
-      ++i
     }
-  
-    return answer * 2
-    // 여기서 많이 시간을 소요했다.
-    // 두개가 같이 사라지므로 두배를 해야 한다.
+
+    ++i
   }
-  
-  export { solution as getRemovedDollsCount }
-  
+
+  return answer
+  // 여기서 많이 시간을 소요했다.
+  // 두개가 같이 사라지므로 두배를 해야 한다.
+}
+
+export { solution as getRemovedDollsCount }
