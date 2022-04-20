7aceefdaab16ebf71db90f3d2ad688f1bf60d9e4
suyeon woo <woosean999@gmail.com>
2022-04-20 12:01:40 +0900

    정답 참고 및 세번째 풀이

--- src/strings/mixedCharacters.ts
+++ src/strings/mixedCharacters.ts
@@ -2,42 +2,27 @@
 2021 카카오 채용연계형 인턴십 문제 - 숫자 문자열과 영단어
 */
 
-function solution(s) {
-  let arr = s.split('')
-  const group1 = ['z', 'o', 'e', 'n']
-  const group2 = ['t', 'f', 's']
-  const letterProps = {
-    z: { length: 4, n: 0 },
-    o: { length: 3, n: 1 },
-    t: { w: { length: 3, n: 2 }, h: { length: 5, n: 3 } },
-    f: { o: { length: 4, n: 4, i: { length: 4, n: 5 } } },
-    s: { i: { length: 3, n: 6 }, e: { length: 5, n: 7 } },
-    e: { length: 5, n: 8 },
-    n: { length: 4, n: 9 },
+function solution(s: string) {
+  let numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ]
+  var answer = s
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i])
+    answer = arr.join(i.toString())
   }
 
-  let answerStr = ''
-  let i = 0
-  while (i < arr.length) {
-    if (group1.includes(arr[i])) {
-      const prop = letterProps[arr[i]]
-      if (prop) {
-        answerStr += prop['n']
-        i += prop['length']
-      }
-    } else if (group2.includes(arr[i])) {
-      const prop = letterProps[arr[i]][arr[i + 1]]
-      if (prop) {
-        answerStr += prop['n']
-        i += prop['length']
-      }
-    } else {
-      answerStr += arr[i]
-      i++
-    }
-  }
-
-  return parseInt(answerStr)
+  return parseInt(answer)
 }
 
 export { solution as convertMixedCharactersIntoNumbers }
