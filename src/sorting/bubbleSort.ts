ff0c483858c314c7087cb24d0270bbda4b0f25fb
suyeon woo <woosean999@gmail.com>
2022-04-20 09:27:27 +0900

    원소 교체 방식 변경

--- src/sorting/bubbleSort.ts
+++ src/sorting/bubbleSort.ts
@@ -11,9 +11,10 @@ function solution(arr: number[]): void {
   for (let i = 0; i < arr.length; ++i) {
     for (let j = 0; j < arr.length - i - 1; ++j) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
+        // const temp = arr[j]
+        // arr[j] = arr[j + 1]
+        // arr[j + 1] = temp
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
     }
   }
