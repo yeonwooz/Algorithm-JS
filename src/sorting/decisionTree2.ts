43d7fa01cf95d2171cd441c5ee9f0da8afdc7070
suyeon woo <woosean999@gmail.com>
2022-04-22 11:25:38 +0900

    결정 알고리즘 2  해설 참고 풀이 - 정해진 최대갭을 기준으로 모든 말을 배치할 수 있는지 카운트

--- src/sorting/decisionTree2.ts
+++ src/sorting/decisionTree2.ts
@@ -1,28 +1,37 @@
-// count: 말의 마리수 , arr: 마구간들 각각의 좌표, 중복 없음. (수직선상에 위치함)
-function solution(count: number, arr: number[]) {
+function count(arr: number[], dist: number) {
+  let count = 1
+  let ep = arr[0]
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] - ep >= dist) {
+      count++
+      ep = arr[i]
+    }
+  }
+  return count
+}
+
+// c: 말의 마리수 , arr: 마구간들 각각의 좌표, 중복 없음. (수직선상에 위치함)
+function solution(c: number, arr: number[]) {
   // 구해야 하는 것 : 가장 가까운 두 말의 최대거리
 
   arr.sort((a, b) => a - b)
-  let diffs = []
-  for (let i = 0; i < arr.length - 1; ++i) {
-    diffs.push(Math.abs(arr[i] - arr[i + 1]))
-  }
-  let lt = Math.min(...diffs)
-  let rt = Math.max(...diffs)
+  let lt = 1
+  let rt = arr[arr.length - 1]
 
-  let nearestLength = lt
+  let nearest = lt
   while (lt <= rt) {
     let mid = Math.floor((lt + rt) / 2)
 
-    if (nearestLength <= mid) {
-      nearestLength = mid
-      rt = mid - 1
-    } else {
+    if (count(arr, mid) >= c) {
+      nearest = mid
       lt = mid + 1
+    } else {
+      rt = mid - 1
     }
   }
 
-  return nearestLength
+  return nearest
 }
 
 export { solution as decisionTree2 }
