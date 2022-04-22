7a98c264e373abf7cd4d9aa2a4de2e08fedb7a32
suyeon woo <woosean999@gmail.com>
2022-04-22 12:17:23 +0900

    이진재귀 간결화

--- src/recursion/binaryRecursion.ts
+++ src/recursion/binaryRecursion.ts
@@ -1,11 +1,8 @@
 function solution(n: number): number | string {
   if (n / 2 <= 1) {
-    const a = n / 2
-    const b = n % 2
-    return a.toString() + b.toString()
+    return String(n / 2) + String(n % 2)
   } else {
-    const c = solution(Math.floor(n / 2)) + (n % 2).toString()
-    return parseInt(c)
+    return parseInt(solution(Math.floor(n / 2)) + String(n % 2))
   }
 }
 
@@ -23,4 +20,4 @@ function solution2(n: number): number | string {
   DFS(n)
   return parseInt(answer)
 }
-export { solution2 as binaryRecursion }
+export { solution as binaryRecursion }
