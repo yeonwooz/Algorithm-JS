8d6dec18fdd13cd081dd10814f4019bc9d3da7a7
suyeon woo <woosean999@gmail.com>
2022-04-22 09:31:05 +0900

    결정 알고리즘 해결 - 구하려는 값이 범위 안에서 이진탐색을 하다가 조건에 부합하면 리턴한다

--- src/sorting/decisionTree.ts
+++ src/sorting/decisionTree.ts
@@ -1,6 +1,21 @@
 // n : 전체 곡의 수 , m: DVD수, arr: 차례대로 곡의 길이
 
-function solution(n: number, m: number, arr: number[]) {
+function count(songs: number[], capacity: number): number {
+  let currentDvdSize = 0
+  let count = 1
+
+  for (let i = 0; i < songs.length; ++i) {
+    if (currentDvdSize + songs[i] <= capacity) {
+      currentDvdSize += songs[i]
+    } else if (currentDvdSize + songs[i] > capacity) {
+      count++
+      currentDvdSize = songs[i]
+    }
+  }
+  return count
+}
+
+function solution(n: number, m: number, arr: number[]): number {
   // 구하려는 값: 최소 dvdSize
   // 구하려는 값의 범위(이진탐색범위) : arr의 최대값 이상 sum 이하
 
@@ -11,13 +26,9 @@ function solution(n: number, m: number, arr: number[]) {
   let rt = sum
   let dvdSize
   while (lt <= rt) {
-    let avgSongSize = Math.ceil(sum / m)
     let mid = Math.floor((lt + rt) / 2)
 
-    if (avgSongSize === mid) {
-      dvdSize = mid
-      break
-    } else if (avgSongSize < mid) {
+    if (count(arr, mid) <= m) {
       dvdSize = mid
       rt = mid - 1
     } else {
