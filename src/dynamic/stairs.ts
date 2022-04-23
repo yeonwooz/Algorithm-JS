682c210d1d8e47b1584d1b3b6f06aad6dc17eebc
suyeon woo <woosean999@gmail.com>
2022-04-23 18:20:54 +0900

    동적계획법 - 경우 쪼개기 / 피보나치 방식으로 계단오르는 경우의 수 구하기

--- src/dynamic/stairs.ts
+++ src/dynamic/stairs.ts
@@ -5,20 +5,23 @@ Q. Get the number of ways to go upstairs through 'n' stairs.
 For each step you can go one or two stairs at once.
 */
 
-
-
 function solution(n: number): number {
-    /*
+  /*
     a(n) = n칸오르기
-    a(5) = (a(1) + a(4) 
-          + a(2) + a(3)
-          + a(3) + a(2)
-          + a(4) + a(1)) / 2
+    a(n) 은 a(n-1) + a(n-2)
     */
 
-    
+  if (n <= 2) {
+    return 1
+  }
+  if (n === 3) {
+    return 3
+  }
+  if (n === 4) {
+    return 5
+  }
 
-  
+  return solution(n - 1) + solution(n - 2)
 }
 
 export { solution as stairs }
