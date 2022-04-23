d52a4a33dd365ddb9364e8b3f0cb245d3df0b144
suyeon woo <woosean999@gmail.com>
2022-04-23 09:38:11 +0900

    두번째 아이디어 - 앞의 경우의 수 조합

--- src/dynamic/stairs.ts
+++ src/dynamic/stairs.ts
@@ -5,30 +5,20 @@ Q. Get the number of ways to go upstairs through 'n' stairs.
 For each step you can go one or two stairs at once.
 */
 
-function solution(n: number) {
-  // when pass every third step
-  const countOfThird = Math.floor(n / 3)
-  const countOfThirdRest = n % 3
 
-  const waysToFinishByThird = countOfThird * 3
-  let totalWays = waysToFinishByThird
-  if (countOfThirdRest === 1) {
-    totalWays += 1
-  } else if (countOfThirdRest === 2) {
-    totalWays += 2
-  }
 
-  // when not passing every third step
-  const countOfSecond = Math.floor(n / 2)
-  const countOfSecondRest = n % 2
-  const waysToFinishBySecond = countOfSecond
-  totalWays += waysToFinishBySecond + countOfSecondRest
+function solution(n: number): number {
+    /*
+    a(n) = n칸오르기
+    a(5) = (a(1) + a(4) 
+          + a(2) + a(3)
+          + a(3) + a(2)
+          + a(4) + a(1)) / 2
+    */
 
-  const countOfSixth = Math.floor(n / 6)
-  const countOfSecondSi = n % 2
+    
 
-  totalWays -= countOfSixth
-  return totalWays
+  
 }
 
 export { solution as stairs }
