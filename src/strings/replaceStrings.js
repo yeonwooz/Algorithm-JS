978640b6f729d131d655d97ae25b8da614472b7f
suyeon woo <woosean999@gmail.com>
2022-04-29 14:28:25 +0900

    join 사용

--- src/strings/replaceStrings.js
+++ src/strings/replaceStrings.js
@@ -2,8 +2,6 @@
 2021 Kakao internship 
 */
 
-
-
 // replace / replaceAll 치환
 
 function solution(s) {
@@ -24,11 +22,13 @@ function solution(s) {
 
   for (let i = 0; i < 10; ++i) {
     // answer = answer.replaceAll(dicts[i], i)
-    // replaceAll은 최신스펙이므로 사용할 수 없는 경우도 있다 
-    for (let j = 0; j < answer.length; ++j) {
-        // 불필요하게 많이 반복하여 메모리 낭비 발생
-      answer = answer.replace(dicts[i], i)
-    }
+    // replaceAll은 최신스펙이므로 사용할 수 없는 경우도 있다
+    // for (let j = 0; j < answer.length; ++j) {
+    //     // 불필요하게 많이 반복하여 메모리 낭비 발생
+    //   answer = answer.replace(dicts[i], i)
+    // }
+    const splitted = answer.split(dicts[i])
+    answer = splitted.join(i)
   }
 
   return parseInt(answer)
